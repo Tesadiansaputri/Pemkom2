8fafe62af3c9b8df9df442dbc8dd118b57b3bb40
ADVAN <ADVAN@DESKTOP-G17K6A9>
2026-06-18 19:58:27 +0700

    feat: tambah SerialService & DigitalClockService

---
+++ src/main/java/services/LogAbsensiService.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package services;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+import model.GenericDAO;
+import model.LogAbsensi;
+
+/**
+ *
+ * @author ADVAN
+ */
+public class LogAbsensiService {
+    private final GenericDAO<LogAbsensi> logDAO = new GenericDAO<>("log_absensi", LogAbsensi.class);
+    
+    public void simpanLog(String hashedUid, String status) {
+        // Membuat objek LogAbsensi sesuai parameter di sumber [6]
+        LogAbsensi log = new LogAbsensi(
+            UUID.randomUUID().toString(), 
+            hashedUid, 
+            LocalDateTime.now(), 
+            status
+        );
+        logDAO.save(log); // Menyimpan ke MongoDB [7]
+    }
+}
